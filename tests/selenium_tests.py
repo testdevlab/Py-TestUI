d7ee836f068a0e22e3079bbb390aba31aee31ab6
Álvaro Santos Laserna López <alvaro.lasernalopez@testdevlab.com>
2020-02-20 18:11:54 +0200

    add some more stuff

--- tests/selenium_tests.py
+++ tests/selenium_tests.py
@@ -19,8 +19,6 @@ class TestStringMethods(object):
     @pytest.mark.signup
     def test_sign_up_flow(self, selenium_driver: TestUIDriver):
         logger.log_test_name("T92701: Create an account")
-        selenium_driver.navigate_to('https://app.bitrise.io/artifact/30283321/p/eb48e3bff79dec86a9257dfe9de3ac6c')
-        sleep(5)
-        # landing_page = LandingScreen(selenium_driver)
-        # landing_page.i_am_in_landing_screen()
-        # selenium_driver.raise_errors()
+        landing_page = LandingScreen(selenium_driver)
+        landing_page.i_am_in_landing_screen()
+        selenium_driver.raise_errors()
