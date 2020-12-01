72ff6cc49e5cccebf3289bacf19547cdcd349fb6
Álvaro Santos Laserna López <alvaro.lasernalopez@testdevlab.com>
2020-12-01 13:38:33 +0200

    fix naming pattern

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="py-testui",
+    name="py_testui",
     author="Alvaro Santos Laserna Lopez",
     version="0.1.0",
     url="https://testdevlab.com",
