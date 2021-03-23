ab7887c4fbacc53f0aaed6cf112d3451600c6a24
Alekss Litvinovs <alekss.litvinovs@testdevlab.com>
2021-03-23 20:42:44 +0200

    Add include_stacktrace and include_screenshot params for testui_error()

--- tests/appium_tests.py
+++ tests/appium_tests.py
@@ -6,18 +6,24 @@ from testui.support.appium_driver import NewDriver
 from testui.support.testui_driver import TestUIDriver
 
 
-class TestStringMethods(object):
-    @pytest.yield_fixture(autouse=True)
-    def selenium_driver(self):
-        driver = NewDriver() \
-            .set_logger().set_chrome_driver().set_soft_assert(True).set_appium_driver()
-        yield driver
-        driver.quit()
+@pytest.yield_fixture(autouse=True)
+def driver() -> TestUIDriver:
+    driver = (
+        NewDriver()
+        .set_logger()
+        .set_chrome_driver()
+        .set_soft_assert(True)
+        .set_appium_driver()
+    )
 
-    @pytest.mark.signup
-    def test_sign_up_flow(self, selenium_driver: TestUIDriver):
-        logger.log_test_name("T92701: Create an account")
-        selenium_driver.navigate_to('https://google.com')
-        landing_page = LandingScreen(selenium_driver)
-        landing_page.i_am_in_mobile_landing_screen()
-        selenium_driver.raise_errors()
+    yield driver
+
+    driver.quit()
+
+@pytest.mark.signup
+def test_appium(driver: TestUIDriver):
+    logger.log_test_name("T92701: Create an account")
+    driver.navigate_to('https://google.com')
+    landing_page = LandingScreen(driver)
+    landing_page.i_am_in_mobile_landing_screen()
+    driver.raise_errors()
--- tests/selenium_tests.py
+++ tests/selenium_tests.py
@@ -1,6 +1,5 @@
-from time import sleep
-
 import pytest
+
 from selenium.webdriver.chrome.options import Options
 
 from tests.screens.landing import LandingScreen
@@ -9,21 +8,30 @@ from testui.support.appium_driver import NewDriver
 from testui.support.testui_driver import TestUIDriver
 
 
-class TestStringMethods(object):
-    @pytest.yield_fixture(autouse=True)
-    def selenium_driver(self):
-        options = Options()
-        options.add_argument("disable-user-media-security")
-        driver = NewDriver() \
-            .set_logger().set_browser('chrome').set_remote_url("http://localhost:4444/wd/hub").set_soft_assert(True) \
-            .set_selenium_driver(chrome_options=options)
-        yield driver
-        driver.quit()
+@pytest.fixture(autouse=True)
+def driver() -> TestUIDriver:
+    options = Options()
+    options.add_argument("disable-user-media-security")
+    
+    driver = (
+        NewDriver()
+        .set_logger()
+        .set_browser('chrome')
+        .set_remote_url("http://localhost:4444/wd/hub")
+        .set_soft_assert(True) 
+        .set_selenium_driver(chrome_options=options)
+    )
 
-    @pytest.mark.signup
-    def test_sign_up_flow(self, selenium_driver: TestUIDriver):
-        logger.log_test_name("T92701: Create an account")
-        selenium_driver.navigate_to('https://google.com')
-        landing_page = LandingScreen(selenium_driver)
-        landing_page.i_am_in_landing_screen()
-        selenium_driver.raise_errors()
+    yield driver
+    
+    driver.quit()
+
+@pytest.mark.signup
+def test_sign_up_flow(driver: TestUIDriver):
+    logger.log_test_name("T92701: Create an account")
+    
+    driver.navigate_to('https://google.com')
+    landing_page = LandingScreen(driver)
+    landing_page.i_am_in_landing_screen()
+    
+    driver.raise_errors()
