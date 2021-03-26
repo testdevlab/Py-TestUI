c8c88e5a033ef9db544f26f1766e38115ae6764e
alvarolaserna <48132162+alvarolaserna@users.noreply.github.com>
2021-03-26 14:19:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name="py_testui",
     author="Alvaro Santos Laserna Lopez",
-    version="1.0.4",
+    version="1.0.5",
     url="https://testdevlab.com",
     long_description=long_description,
     packages=find_packages(),
